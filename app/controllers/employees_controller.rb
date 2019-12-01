f3884de16b523f29665f05e8ead59e847f13bd55
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-01 17:08:14 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,19 @@
 class EmployeesController < ApplicationController
+    def index 
+        @employees = Employee.all
+    end 
+    def show
+        @employee = Employee.find(params[:id])
+    end 
+    def new 
+        @employee = Employee.new 
+    end 
+    def create 
+        @employee = Employee.create(params.require(:employee).permit(:office, :first_name, :last_name, :alias, :title, :img_url, :dog_id))
+        if @employee
+            redirect_to employee_path(@employee)
+        else
+            render :new 
+        end 
+    end 
 end
